ae46e36f35670fd10ae05a476ed5c5fa5aa0f3d3
saurabh201189 <saurabh201189@gmail.com>
2014-07-22 18:20:32 +0530

    Created sample rails application grt_user_info

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GrtUserInfo::Application.config.secret_token = '5d622772dcdec59c1adff29a32fbc6675ee950685219d114481b325d29a95f2ad0526a5c5d6be8e83911b608245c716dfc2fea71ef41051544bb663cb818f769'
