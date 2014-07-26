2448590644cefb749aa6662aadbbf3efa5df49ea
Saurabh Pandit <saurabh@saurabh-K55VM.(none)>
2014-07-27 00:19:00 +0530

    Added the test cases for the application.

--- test/functional/user_detail_controller_test.rb
+++ test/functional/user_detail_controller_test.rb
@@ -7,19 +7,40 @@ class UserDetailControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test 'new should render correct template and layout' do
+    get :new
+    assert_template :new
+    assert_template layout: 'application'
+  end
+
   test 'should get details' do
-    get :details
+    get :details, :user_name => 'sferik'
     assert_response :success
+    assert_equal 'sferik', assigns('user_name')
+  end
+
+  test 'details should render correct template and layout' do
+    get :details
+    assert_template :details
+    assert_template layout: 'application'
   end
 
   test 'should export details in pdf' do
-    get :export, :mime_type => 'pdf'
+    get :export, :mime_type => 'pdf', :user_name => 'sferik'
     assert_response :success
+    assert_equal 'sferik', assigns('user_name')
   end
 
   test 'should export details in doc' do
-    get :export, :mime_type => 'doc'
+    get :export, :mime_type => 'doc', :user_name => 'sferik'
     assert_response :success
+    assert_equal 'sferik', assigns('user_name')
+  end
+
+  test 'export should render correct template and layout' do
+    get :export, :mime_type => 'pdf'
+    assert_template :export
+    assert_template layout: 'export'
   end
 
 end
